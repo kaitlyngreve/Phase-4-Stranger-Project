f9bf0274604cffe022e3e259cf6a2b851575ab24
Kaitlyn Greve <kgreve14@gmail.com>
2022-06-22 18:41:13 -0400

    play again buttons

--- client/src/Lose.js
+++ client/src/Lose.js
@@ -1,12 +1,23 @@
 import { useState, useEffect } from "react";
 // import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 function Lose() {
+
+    const history = useHistory()
+
+    function handlePlayAgain(e) {
+        e.preventDefault()
+        history.push("/characters")
+    }
 
     return (
         <>
             <div>
                 <h1>YOU DIE</h1>
+            </div>
+            <div>
+                <button onClick={handlePlayAgain} >FIGHT AGAIN</button>
             </div>
         </>
     );
--- client/src/Win.js
+++ client/src/Win.js
@@ -1,12 +1,23 @@
 import { useState, useEffect } from "react";
 // import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 function Win() {
+
+    const history = useHistory()
+
+    function handlePlayAgain(e) {
+        e.preventDefault()
+        history.push("/characters")
+    }
 
     return (
         <>
             <div>
                 <h1>YOU DEFEATED THE MONSTER</h1>
+            </div>
+            <div>
+                <button onClick={handlePlayAgain} >FIGHT AGAIN</button>
             </div>
         </>
     );
